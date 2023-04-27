139defdba1b9e7513ef30b0d6fb92ef5035cf116
Tiffany Palermo <tiffany.palermo@faircg.com>
2023-04-27 19:26:33 +1000

    Your commit message

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Card.css';
 
 const Card = ({ image, title, description }) => (
-  // ...
+  <div className="card">
+    <img className="card-image" src={image} alt={title} />
+    <div className="card-content">
+    <h2 className="card-title">{title}</h2>
+    <p className="card-description">{description}</p>
+    </div>
+  </div>
 );
-
 Card.propTypes = {
   image: PropTypes.string,
   title: PropTypes.string.isRequired,
